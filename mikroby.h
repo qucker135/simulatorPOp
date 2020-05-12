fc21ff1b16327b0071c433129a3b41ed730cf6ff
Bodzioss <56918680+Bodzioss@users.noreply.github.com>
2020-05-12 13:48:43 +0200

    Update mikroby.h

--- mikroby.h
+++ mikroby.h
@@ -1,32 +1,16 @@
-#include<iostream>
-
-using namespace std;
-
 class Mikroby
 {
-protected:
-    bool zdolnosc_ruchu;
-    int energia;
-    int min_energia_podzialu;
-    int energia_p;
-    int polee;
-    int pozywienie;
-    int trucizna;
-    int zebrane_H20;
-    int zebrane_CO2;
-    int zebrana_glukoza;
-    int zebrany_tlen;
-    int zebrany_etanol;
-    int zebrany_metanol;
-    int organizm;
-public:
-    Mikroby(int energia, int energia_p, int min_energia_podzialu);
-    ~Mikroby();
-    int get_energia(int polee);
-    void zbierz(int pozywienie, int trucizna);
-    void przerob();
-    void podziel(int x);
-    void skonaj();
+	protected:
+		bool zdolnosc_ruchu;
+		int energia;
+		int minimalna_energia_do_podzialu;
+
+	public:
+		int getEnergia();
+		void zbierz();
+		void przerob();
+		void podziel();
+		void skonaj();
 };
 
 class Samozywne: public Mikroby
@@ -69,6 +53,3 @@ class Reducenci: public Mikroby
 		int getEtanol();
 		int getMetan();
 };
-
-
-void nieZnika(int x, int y);
